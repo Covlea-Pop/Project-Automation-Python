e65ed0d9f499ab67e4b61d74759958d733f3fd57
Covlea-Pop <covleacc@gmail.com>
2021-11-08 22:09:28 +0200

    new

---
+++ features/steps/dynamic_con.py
@@ -0,0 +1,33 @@
+import time
+
+from behave import *
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+@when('I access Dynamic Controls page')
+def step_impl(context):
+    context.driver.get("https://the-internet.herokuapp.com/")
+    context.driver.maximize_window()
+    context.driver.find_element(By.CSS_SELECTOR, "#content > ul > li:nth-child(13) > a").click()
+    time.sleep(2)
+
+
+@when('I can use all features from Dynamic Control')
+def step_impl(context):
+    context.driver.find_element(By.CSS_SELECTOR, "#checkbox > input[type=checkbox]").click()
+    time.sleep(5)
+    context.driver.find_element(By.CSS_SELECTOR, "#checkbox-example > button").click()
+    time.sleep(5)
+    context.driver.find_element(By.CSS_SELECTOR, "#checkbox-example > button").click()
+    time.sleep(5)
+    context.driver.find_element(By.CSS_SELECTOR, "#checkbox").click()
+    time.sleep(3)
+    context.driver.find_element(By.CSS_SELECTOR, "#input-example > button").click()
+    time.sleep(3)
+    textfield = context.driver.find_element(By.CSS_SELECTOR, "#input-example > input[type=text]")
+    textfield.send_keys("Test")
+    time.sleep(5)
+    context.driver.find_element(By.CSS_SELECTOR, "#input-example > button").click()
+    time.sleep(5)
+    context.driver.back()

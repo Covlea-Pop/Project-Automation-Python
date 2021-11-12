85c9362941dcd0b823245be27c10e266bbe9fcdd
Covlea-Pop <covleacc@gmail.com>
2021-11-13 00:42:38 +0200

    new

--- features/steps/dynamic_loa.py
+++ features/steps/dynamic_loa.py
@@ -2,8 +2,7 @@ import time
 
 from behave import *
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 
 
 @when('I access Dynamic Loading page')
